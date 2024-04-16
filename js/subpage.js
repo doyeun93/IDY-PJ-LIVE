404739b36e740638c044fafe4dc116d66d619827
doyeun93 <doyeun93@naver.com>
2024-04-16 13:15:15 +0900

    s

--- js/subpage.js
+++ js/subpage.js
@@ -1,4 +1,7 @@
 import {icMenuData} from '../data/ic_data.js';
+import loadFn from './common.js';
+loadFn();
+
 
 const qs = (x) => document.querySelector(x);
 const qsa = (x) => document.querySelectorAll(x);
